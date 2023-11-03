41aee52b36d4764f120efe9b7c6178671e8748ec
Đỗ Văn Hoan <vanhoan90it6@gmail.com>
2023-11-03 16:55:28 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {Post} from "../models/post";
+import {ApiService} from "../services/ApiService";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'gm-fe';
+  posts$: Observable<Post[]> = new Observable<Post[]>();
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.posts$ = this.apiService.getPosts();
+  }
 }
---
+++ src/models/post.ts
@@ -0,0 +1,6 @@
+export interface Post {
+  id: number;
+  thumbnailUrl: string;
+  title: string;
+  price: number;
+}
